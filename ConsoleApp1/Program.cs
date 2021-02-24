8a153fdb4573a16182c5107a78302a82273e0c79
=Yanush <giroskop976@gmail.com>
2021-02-24 14:54:21 +0200

    1 Commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Goodbye");
+            Console.WriteLine("Goodbye!");
         }
     }
 }
