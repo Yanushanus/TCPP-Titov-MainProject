aa0f774cc99ef15710b1c70ebdf304beb2f63a4f
Yanushanus <60926887+Yanushanus@users.noreply.github.com>
2021-03-11 10:18:29 +0200

    changed from browser

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World from browser!");
             Console.WriteLine("How are you?!");
             Console.WriteLine("How old are you!");
             Console.WriteLine("Goodbye warriors!");
